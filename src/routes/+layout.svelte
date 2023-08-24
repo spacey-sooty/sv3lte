86ffe656a51c6d7e89a2af985cb4f725259a8229
Isaac Turner <spacey_sooty@outlook.com>
2023-08-24 22:14:35 +0800

    some basic alias and layout work

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script>
-	import '../styles/index.scss';
+	import '@/styles/index.scss';
 </script>
 
 <div>
+  <head>
+    <title> Sv3lte Goofing </title>
+    <meta name="content" content="Love me some sv3lte so I'm goofing around with it!"/>
+  </head>
 	<main>
 		<slot />
 	</main>
